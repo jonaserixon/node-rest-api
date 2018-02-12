20a1246668cee48f5ba0ae60863b22554bd04c02
Jonas Erixon <x.jonaz@gmail.com>
2018-02-12 02:43:24 +0100

    setup passport authentication with github account

--- app.js
+++ app.js
@@ -4,21 +4,57 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let CatchModel = require('./models/catchModel');
 
+
+let passport = require('passport');
+let GitHubStrategy = require('passport-github2').Strategy;
+
+var GITHUB_CLIENT_ID = '672e1847db6e0d151b27';
+var GITHUB_CLIENT_SECRET = 'b1e63aa0c34c7f22bd97ee65bcbe09b992839673';
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:8000/auth/github/callback",
+    scope: ['user:email']
+},
+
+    function(accessToken, refreshToken, profile, done) {
+        process.nextTick(function () {
+            return done(null, profile);
+        });
+    }
+));
+
+
+
 let app = express();
 let port = process.env.PORT || 8000;
+
 
 //database
 require('./config/database').initilize();
 
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(passport.initialize());
 
 //Routes
-let routes = require('./routes/routes'); 
+let routes = require('./routes/routes')(passport); 
 app.use('/', routes);
+
+
+
+
 
 
 //Web server
--- routes/routes.js
+++ routes/routes.js
@@ -4,99 +4,112 @@ let router = require("express").Router();
 let mongoose = require('mongoose');
 let CatchModel = mongoose.model('Catch');
 
-router.route('/')
-    .get(function (req, res){
-        res.send('<p>To use our API, refer to the http://localhost:8000/api/ resource.</p>');
-    })
+module.exports = function(passport) {
 
-router.route('/api/')
-    .get(function (req, res){
-        res.json( { message: 'Welcome to the api!' });
-    })
-
-router.route('/api/catches')
-    .get(function(req, res) {
-        CatchModel.find({}, function(err, doc) {
-            if (err) {
-
-            }
-
-            res.json(doc);
+    router.route('/')
+        .get(function (req, res){
+            res.send('<p>To use our API, refer to the http://localhost:8000/api/ resource.</p>');
         })
-    })
-    .post(function(req, res) {
-        let makeTest = new CatchModel(req.body);
-        makeTest.save(function(err, doc) {
-            if (err) {
 
-            }
+    router.route('/api/')
+        .get(function (req, res){
+            res.json( { message: 'Welcome to the api!' });
+        })
 
-            res.json(doc);
-        });
-    })
+    router.route('/api/catches')
+        .get(function(req, res) {
+            CatchModel.find({}, function(err, doc) {
+                if (err) {
 
+                }
 
-router.route('/api/catches/:id')
-    .get(function (req, res) {
-        CatchModel.findById(req.params.id, function (err, doc){
-            res.json({ 
-                id: doc.id,
-                user: doc.user,
-                position: doc.position,
-                specie: doc.specie,
-                weigth: doc.weigth,
-                length: doc.length,
-                image_url: doc.image_url,
-                description: doc.description,
-                misc: doc.misc,
-                timestamp: doc.timestamp,
-                links: [
-                    {
-                        self: 'http://localhost:8000/api/catches/' + doc.id,
-                        category: 'http://localhost:8000/api/catches/'
-                    }
-                ]
+                res.json(doc);
+            })
+        })
+        .post(function(req, res) {
+            let makeTest = new CatchModel(req.body);
+            makeTest.save(function(err, doc) {
+                if (err) {
+
+                }
+
+                res.json(doc);
             });
-        });
-    })
-    
-    .put(function(req, res) {
-        CatchModel.findByIdAndUpdate(req.params.id, function(err, doc) {
-            if (err) {
-                //error message
-                res.status(400).json(err);
-            }
+        })
 
-            res.json({ 
-                message: 'Update successful!',
-                user: doc.user,
-                links: [
-                    {
-                        self: 'http://localhost:8000/api/catches/' + doc.id,
-                        category: 'http://localhost:8000/api/catches/'
-                    }
-                ]
+
+    router.route('/api/catches/:id')
+        .get(function (req, res) {
+            CatchModel.findById(req.params.id, function (err, doc){
+                res.json({ 
+                    id: doc.id,
+                    user: doc.user,
+                    position: doc.position,
+                    specie: doc.specie,
+                    weigth: doc.weigth,
+                    length: doc.length,
+                    image_url: doc.image_url,
+                    description: doc.description,
+                    misc: doc.misc,
+                    timestamp: doc.timestamp,
+                    links: [
+                        {
+                            self: 'http://localhost:8000/api/catches/' + doc.id,
+                            category: 'http://localhost:8000/api/catches/'
+                        }
+                    ]
+                });
             });
-        });
-    })
+        })
 
-    .delete(function(req, res) {
-        CatchModel.findByIdAndRemove(req.params.id, function(err) {
-            if (err) {
-                //error message
-                res.status(400).json(err);
-            }
+        .put(function(req, res) {
+            CatchModel.findByIdAndUpdate(req.params.id, function(err, doc) {
+                if (err) {
+                    //error message
+                    res.status(400).json(err);
+                }
 
-            res.json({
-                message: 'Delete success!'
+                res.json({ 
+                    message: 'Update successful!',
+                    user: doc.user,
+                    links: [
+                        {
+                            self: 'http://localhost:8000/api/catches/' + doc.id,
+                            category: 'http://localhost:8000/api/catches/'
+                        }
+                    ]
+                });
             });
-        });
-    })
+        })
+
+        .delete(function(req, res) {
+            CatchModel.findByIdAndRemove(req.params.id, function(err) {
+                if (err) {
+                    //error message
+                    res.status(400).json(err);
+                }
+
+                res.json({
+                    message: 'Delete success!'
+                });
+            });
+        })
+
+        // let passport = require('passport');
+        // let GitHubStrategy = require('passport-github2').Strategy;
+
+        router.get('/auth/github',
+        passport.authenticate('github', { scope: [ 'user:email' ] }),
+            function(req, res){}
+        );
+
+    router.get('/auth/github/callback', 
+        passport.authenticate('github', { failureRedirect: '/login' }),
+            function(req, res) {
+                res.redirect('/');
+            }
+    );
 
 
-router.route('/api/auth')
-    .get(function(req, res) {
-        //github login för att få access till api
-    })
-
-module.exports = router;
+    return router;
+};
